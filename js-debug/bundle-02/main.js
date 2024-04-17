d571e739e65e25d0022d57b864bd18ed205ad852
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-04-18 01:06:12 +0200

    bundle02 finished

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -11,57 +11,72 @@
 
 
 // ESERCIZIO 1
-function checkAge() {
-    const myAge = 32;
-    const message = '';
+// function checkAge() {
+//     const myAge = 32;
+//     const message = '';
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
-    } else {
-        message = 'Hai più di 18 anni!';
-    }
-}
-checkAge();
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+// }
+// checkAge();
 
-// ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// 1- l'esercizio vuole stampare un messaggio in base all'età per dire se sei minorenne o maggiorenne
 
+// 2- gli errori sono:
+// riga 16 = le variabili devono avere let e non const per poter essere modificate
+// riga 18, il segno < deve essere >
 
-// ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// // ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
-
-
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
-
-    let grantAccess = 'false';
-
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+// 1- la funzione vuole stampare il numero totale di colori nell'array
+// 2- invece di scrivere colors.lenght dobbiamo scriverre color.length
 
 
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+
+// 1- la funzione vuole sommare +12 al numero inserito
+// 2- i problemi sono:
+// riga 45 = il prompt deve avere parseInt per trasformare l'input da stringa a numero e poi sommarlo
+
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = 'false';
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
+
+// 1- la funzione controlla se l'email inserita è esatta 
+// 2- i true ed i false non vanno scritti tra virgolette
+
+
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -88,6 +103,9 @@ function checkAccessImproved() {
         }
     }
     checkAccessImproved();
+    
+// 1- la funzione controlla se la tua email è più lunga di 5 lettere oltre ad essere presente nel database
+// 2- l'errore è che manca la parentesi finale di chiusura della funzione
 
 
 
